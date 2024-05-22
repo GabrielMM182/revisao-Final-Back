5914c1f43b7e57e074782620f73e465fc6b53ff5
GabrielMM182 <gmm_morais@hotmail.com>
2024-05-22 20:39:58 -0300

    fiz deploy

--- src/index.js
+++ src/index.js
@@ -9,4 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`);
+  });
